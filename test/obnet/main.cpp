b800f564847abb9421d786376ebc5f78bf10e0ad
YifanShenSZ <yshen57@jhu.edu>
2021-02-18 00:01:22 -0500

    more thorough test

--- test/obnet/main.cpp
+++ test/obnet/main.cpp
@@ -1,38 +1,36 @@
 #include <CppLibrary/chemistry.hpp>
 
+#include <tchem/utility.hpp>
 #include <tchem/SASintcoord.hpp>
 #include <tchem/SApolynomial.hpp>
 
 #include <obnet/symat.hpp>
+
+#include "InputGenerator.hpp"
 
 int main() {
     c10::TensorOptions top = at::TensorOptions().dtype(torch::kFloat64);
     size_t NStates = 2;
 
     tchem::IC::SASICSet sasicset("default", "IntCoordDef", "SAS.in");
-    //std::cerr << sasicset.origin() << '\n';
 
-    CL::utility::matrix<tchem::polynomial::SAPSet *> input_calculators(NStates);
-    for (size_t i = 0; i < NStates; i++)
-    for (size_t j = i; j < NStates; j++)
-    input_calculators[i][j] = new tchem::polynomial::SAPSet(std::to_string(i + 1) + std::to_string(j + 1) + ".in");
+    std::vector<std::string> sapoly_files = {"11.in", "12.in", "22.in"};
+    InputGenerator input_generator(NStates,  sapoly_files);
 
-    obnet::symat Hd_net("net.in");
+    obnet::symat Hd_net("Hd.in");
     Hd_net.to(torch::kFloat64);
-    CL::utility::matrix<size_t> symmetry = Hd_net.symmetry();
 
     CL::chem::xyz<double> geom("min-C1.xyz", true);
     std::vector<double> coords = geom.coords();
     at::Tensor r = at::from_blob(coords.data(), coords.size(), top);
     at::Tensor q = sasicset.IntCoordSet::operator()(r);
-    //std::cerr << q << '\n';
+    std::vector<at::Tensor> qs = sasicset(q);
+    CL::utility::matrix<at::Tensor> xs = input_generator(qs);
 
-    std::vector<at::Tensor> xs = sasicset(q);
-    CL::utility::matrix<at::Tensor> inputs(NStates);
-    for (size_t i = 0; i < NStates; i++)
-    for (size_t j = i; j < NStates; j++)
-    inputs[i][j] = (*input_calculators[i][j])(xs)[symmetry[i][j]];
+    at::Tensor Hd = Hd_net(xs);
+    std::cout << "Hd =\n" << Hd << '\n';
 
-    at::Tensor Hd = Hd_net(inputs);
-    std::cout << Hd << '\n';
+    std::cout << "Number of parameters =\n"
+              << tchem::utility::NParameters(Hd_net.elements->parameters())
+              << '\n';
 }
