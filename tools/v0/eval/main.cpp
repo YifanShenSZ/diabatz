ec32fe3a774e4a9336cd622bb45b1363abe69e55
YifanShenSZ <yshen57@jhu.edu>
2021-02-26 12:19:31 -0500

    support adiabatz

--- tools/v0/eval/main.cpp
+++ tools/v0/eval/main.cpp
@@ -1,6 +1,7 @@
 #include <CppLibrary/argparse.hpp>
-
 #include <CppLibrary/chemistry.hpp>
+
+#include <tchem/linalg.hpp>
 
 #include <Hd/kernel.hpp>
 
@@ -17,6 +18,9 @@ argparse::ArgumentParser parse_args(const size_t & argc, const char ** & argv) {
     parser.add_argument("-c","--checkpoint",     1, false, "a trained Hd parameter to continue with");
     parser.add_argument("-l","--input_layers", '+', false, "network input layer definition files");
     parser.add_argument("-g","--geometry",       1, false, "the geometry to calculate diabatz");
+
+    // optional argument
+    parser.add_argument("-a","--adiabatz");
 
     parser.parse_args(argc, argv);
     return parser;
@@ -51,12 +55,32 @@ int main(size_t argc, const char ** argv) {
     at::Tensor r = at::from_blob(coords.data(), coords.size(), at::TensorOptions().dtype(torch::kFloat64));
     at::Tensor Hd, dHd;
     std::tie(Hd, dHd) = Hdkernel.compute_Hd_dHd(r);
-    std::cout << "Hd =\n" << Hd << '\n';
-    std::cout << "dHd =\n";
-    for (size_t i = 0; i < Hd.size(0); i++)
-    for (size_t j = i; j < Hd.size(0); j++) {
-        std::cout << "State " << i + 1 << "-" << j + 1 << ":\n";
-        print_grad(dHd[i][j], std::cout);
+
+    if (args.gotArgument("adiabatz")) {
+        at::Tensor energy, states;
+        std::tie(energy, states) = Hd.symeig(true);
+        std::cout << "energy =\n" << energy << '\n';
+        at::Tensor dHa = tchem::linalg::UT_sy_U(dHd, states);
+        std::cout << "energy gradients of\n";
+        for (size_t i = 0; i < Hd.size(0); i++) {
+            std::cout << "state " << i + 1 << ":\n";
+            print_grad(dHa[i][i], std::cout);
+        }
+        std::cout << "nonadiabatic couplings between\n";
+        for (size_t i = 0    ; i < Hd.size(0); i++)
+        for (size_t j = i + 1; j < Hd.size(0); j++) {
+            std::cout << "state " << i + 1 << " and " << j + 1 << ":\n";
+            print_grad(dHa[i][j] / (energy[j] - energy[i]), std::cout);
+        }
+    }
+    else {
+        std::cout << "Hd =\n" << Hd << '\n';
+        std::cout << "▽Hd =\n";
+        for (size_t i = 0; i < Hd.size(0); i++)
+        for (size_t j = i; j < Hd.size(0); j++) {
+            std::cout << "state " << i + 1 << "-" << j + 1 << ":\n";
+            print_grad(dHd[i][j], std::cout);
+        }
     }
 
     std::cout << '\n';
