8829552c2d2f7c3efadabb9617962b732ddd3582
YifanShenSZ <yshen57@jhu.edu>
2021-06-02 22:42:51 -0400

    improve format

--- tools/eval/main.cpp
+++ tools/eval/main.cpp
@@ -21,12 +21,30 @@ argparse::ArgumentParser parse_args(const size_t & argc, const char ** & argv) {
     return parser;
 }
 
-void print_grad(const at::Tensor & grad, std::ostream & ostream) {
-    at::Tensor r = grad.view({grad.size(0) / 3, 3});
-    for (size_t i = 0; i < grad.size(0) / 3; i++)
-    ostream << r[i][0].item<double>() << ' '
-            << r[i][1].item<double>() << ' '
-            << r[i][2].item<double>() << '\n';
+void print_cartvec(const at::Tensor & cartvec, std::ostream & ostream) {
+    at::Tensor r = cartvec.view({cartvec.size(0) / 3, 3});
+    for (size_t i = 0; i < cartvec.size(0) / 3; i++)
+    ostream << std::setw(16) << std::scientific << std::setprecision(6) << r[i][0].item<double>()
+            << std::setw(16) << std::scientific << std::setprecision(6) << r[i][1].item<double>()
+            << std::setw(16) << std::scientific << std::setprecision(6) << r[i][2].item<double>()
+            << '\n';
+}
+
+void print_matrix(const at::Tensor & matrix, std::ostream & ostream) {
+    for (size_t i = 0; i < matrix.size(0); i++) {
+        for (size_t j = 0; j < matrix.size(0); j++)
+        ostream << std::setw(16) << std::scientific << std::setprecision(6) << matrix[i][j].item<double>();
+        ostream << '\n';
+    }
+}
+
+void print_symat(const at::Tensor & matrix, std::ostream & ostream) {
+    for (size_t i = 0; i < matrix.size(0); i++) {
+        for (size_t j = 0; j < i; j++) ostream << "                ";
+        for (size_t j = i; j < matrix.size(0); j++)
+        ostream << std::setw(16) << std::scientific << std::setprecision(6) << matrix[i][j].item<double>();
+        ostream << '\n';
+    }
 }
 
 int main(size_t argc, const char ** argv) {
@@ -48,31 +66,44 @@ int main(size_t argc, const char ** argv) {
     if (args.gotArgument("adiabatz")) {
         at::Tensor energy, states;
         std::tie(energy, states) = Hd.symeig(true);
-        std::cout << "energy =\n" << energy << '\n';
+        // energy
+        std::cout << "energy =\n";
+        for (size_t i = 0; i < energy.size(0); i++)
+        std::cout << std::setw(16) << std::scientific << std::setprecision(6) << energy[i].item<double>();
+        std::cout << '\n';
+        // states
+        std::cout << "states are:\n";
+        print_matrix(states, std::cout);
+        std::cout << '\n';
+        // energy gradient
         at::Tensor dHa = tchem::linalg::UT_sy_U(dHd, states);
-        std::cout << "energy gradients of\n";
         for (size_t i = 0; i < Hd.size(0); i++) {
-            std::cout << "state " << i + 1 << ":\n";
-            print_grad(dHa[i][i], std::cout);
+            std::cout << "energy gradient of state " << i + 1 << " =\n";
+            print_cartvec(dHa[i][i], std::cout);
+            std::cout << '\n';
         }
-        std::cout << "nonadiabatic couplings between\n";
+        // nonadiabatic coupling
         for (size_t i = 0    ; i < Hd.size(0); i++)
         for (size_t j = i + 1; j < Hd.size(0); j++) {
-            std::cout << "state " << i + 1 << " and " << j + 1 << ":\n";
-            print_grad(dHa[i][j] / (energy[j] - energy[i]), std::cout);
+            std::cout << "nonadiabatic coupling between state " << i + 1 << " and " << j + 1 << " =\n";
+            print_cartvec(dHa[i][j] / (energy[j] - energy[i]), std::cout);
+            std::cout << '\n';
         }
     }
     else {
-        std::cout << "Hd =\n" << Hd << '\n';
-        std::cout << "▽Hd =\n";
+        // Hd
+        std::cout << "Hd =\n";
+        print_symat(Hd, std::cout);
+        std::cout << '\n';
+        // ▽Hd
         for (size_t i = 0; i < Hd.size(0); i++)
         for (size_t j = i; j < Hd.size(0); j++) {
-            std::cout << "state " << i + 1 << "-" << j + 1 << ":\n";
-            print_grad(dHd[i][j], std::cout);
+            std::cout << "▽Hd " << i + 1 << "-" << j + 1 << " =\n";
+            print_cartvec(dHd[i][j], std::cout);
+            std::cout << '\n';
         }
     }
 
-    std::cout << '\n';
     CL::utility::show_time(std::cout);
     std::cout << "Mission success\n";
 }
