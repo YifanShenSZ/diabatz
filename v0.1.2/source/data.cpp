82e3c30aaaa1be108692ec4cafedbfba10499b07
YifanShenSZ <yshen57@jhu.edu>
2021-12-22 16:32:38 -0500

    parallelize torch optimizer initialization

--- v0.1.2/source/data.cpp
+++ v0.1.2/source/data.cpp
@@ -12,7 +12,10 @@ read_data(const std::vector<std::string> & user_list, const double & zero_point)
     std::shared_ptr<abinitio::DataSet<abinitio::DegSAHam>> stddegset;
     std::tie(stdregset, stddegset) = reader.read_SAHamSet();
     // process data
-    std::vector<std::shared_ptr<RegHam>> pregs;
+    size_t OMP_NUM_THREADS = omp_get_max_threads();
+    std::vector<std::vector<std::shared_ptr<RegHam>>> pregss(OMP_NUM_THREADS);
+    std::vector<std::vector<std::shared_ptr<DegHam>>> pdegss(OMP_NUM_THREADS);
+    #pragma omp parallel for
     for (size_t i = 0; i < stdregset->size_int(); i++) {
         auto stdreg = stdregset->get(i);
         stdreg->subtract_ZeroPoint(zero_point);
@@ -20,9 +23,10 @@ read_data(const std::vector<std::string> & user_list, const double & zero_point)
         size_t nduplicates = ceil(stdreg->weight());
         stdreg->set_weight(1.0);
         // precompute the input layers
-        for (size_t j = 0; j < nduplicates; j++) pregs.push_back(std::make_shared<RegHam>(stdreg, int2input));
+        int thread = omp_get_thread_num();
+        for (size_t j = 0; j < nduplicates; j++) pregss[thread].push_back(std::make_shared<RegHam>(stdreg, int2input));
     }
-    std::vector<std::shared_ptr<DegHam>> pdegs;
+    #pragma omp parallel for
     for (size_t i = 0; i < stddegset->size_int(); i++) {
         auto stddeg = stddegset->get(i);
         stddeg->subtract_ZeroPoint(zero_point);
@@ -30,7 +34,23 @@ read_data(const std::vector<std::string> & user_list, const double & zero_point)
         size_t nduplicates = ceil(stddeg->weight());
         stddeg->set_weight(1.0);
         // precompute the input layers
-        for (size_t j = 0; j < nduplicates; j++) pdegs.push_back(std::make_shared<DegHam>(stddeg, int2input));
+        int thread = omp_get_thread_num();
+        for (size_t j = 0; j < nduplicates; j++) pdegss[thread].push_back(std::make_shared<DegHam>(stddeg, int2input));
+    }
+    size_t nregs = 0, ndegs = 0;
+    for (size_t i = 0; i < OMP_NUM_THREADS; i++) {
+        nregs += pregss[i].size();
+        ndegs += pdegss[i].size();
+    }
+    std::vector<std::shared_ptr<RegHam>> pregs;
+    std::vector<std::shared_ptr<DegHam>> pdegs;
+    pregs.reserve(nregs);
+    pdegs.reserve(ndegs);
+    for (size_t i = 0; i < OMP_NUM_THREADS; i++) {
+        const auto & pregss_i = pregss[i];
+        const auto & pdegss_i = pdegss[i];
+        pregs.insert(pregs.end(), pregss_i.begin(), pregss_i.end());
+        pdegs.insert(pdegs.end(), pdegss_i.begin(), pdegss_i.end());
     }
     // return
     std::shared_ptr<abinitio::DataSet<RegHam>> regset = std::make_shared<abinitio::DataSet<RegHam>>(pregs);
