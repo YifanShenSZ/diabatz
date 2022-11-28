59dd48c9f441ef00b42e65ecbe288257250be904
YifanShenSZ <yshen57@jhu.edu>
2022-11-27 22:55:36 -0500

    add v1.3.3 to build scripts

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 for directory in \
 v0.0.0 v0.0.1 v0.0.2 v0.1.0 v0.2.0 v0.3.0 v0.3.2 \
-v1.0.0 v1.1.0 v1.3.0 v1.3.2; do
+v1.0.0 v1.1.0 v1.3.0 v1.3.2 v1.3.3; do
     echo
     echo "Entre "$directory
     cd $directory
