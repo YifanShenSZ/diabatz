7a685592e4ebec27e36fb4714646c819a7abece5
YifanShenSZ <yshen57@jhu.edu>
2022-11-27 22:55:57 -0500

    add v1 test

---
+++ test/v1/input-layer/link_layers.sh
@@ -0,0 +1,31 @@
+ln -s $1/1-1.in 11-1.in
+ln -s $1/1-1.in 22-1.in
+ln -s $1/1-1.in 33-1.in
+ln -s $1/1-1.in 44-1.in
+
+ln -s $1/7-1.in 12-1.in
+ln -s $1/7-1.in 34-1.in
+
+ln -s $1/3-1.in 13-1.in
+ln -s $1/3-1.in 24-1.in
+
+ln -s $1/4-1.in 14-1.in
+ln -s $1/4-1.in 23-1.in
+
+
+
+
+
+ln -s $1/1-2.in 11-2.in
+ln -s $1/1-2.in 22-2.in
+ln -s $1/1-2.in 33-2.in
+ln -s $1/1-2.in 44-2.in
+
+ln -s $1/7-2.in 12-2.in
+ln -s $1/7-2.in 34-2.in
+
+ln -s $1/3-2.in 13-2.in
+ln -s $1/3-2.in 24-2.in
+
+ln -s $1/4-2.in 14-2.in
+ln -s $1/4-2.in 23-2.in
