4ca4c084f1c6b543e64048db1d2951292d5f5961
YifanShenSZ <yshen57@jhu.edu>
2022-11-29 23:01:41 -0500

    no longer remove built in rebuild.sh

--- library/rebuild.sh
+++ library/rebuild.sh
@@ -2,7 +2,6 @@ for directory in abinitio SASDIC DimRed obnet Hderiva; do
     echo
     echo "Entre "$directory
     cd $directory/build
-    rm lib*
     cmake --build .
     cd ../..
 done
--- library/retest.sh
+++ library/retest.sh
@@ -7,7 +7,6 @@ for directory in SASDIC DimRed obnet Hderiva; do
     echo
     echo "Entre "$directory"/test"
     cd $directory/test/build
-    rm test.exe
     cmake --build .
     cd ..
     if [ -d input ]; then
