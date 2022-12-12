b6c820512d732994d823ad54503645a0c6ab1d4b
YifanShenSZ <yshen57@jhu.edu>
2022-12-12 14:20:36 -0500

    support more hidden neurons

--- tools/v0/script/filter_polynomial.py
+++ tools/v0/script/filter_polynomial.py
@@ -6,6 +6,7 @@ copy the source parameters to the destination polynomials
 import argparse
 from pathlib import Path
 from typing import Dict
+import numpy as np
 
 from utility import hash_polynomial
 
@@ -27,33 +28,43 @@ def read_polynomial(polynomial_file: Path) -> Dict:
     return hashvalue2polynomial
 
 def read_parameter(parameter_file: Path) -> Dict:
-    hashvalue2parameter = {}
+    hashvalue2parameters = {}
     with open(parameter_file, 'r') as f: lines = f.readlines()
     for i in range(0, len(lines), 2):
         hash_value = hash_polynomial(lines[i])
-        parameter = float(lines[i + 1])
-        if hash_value not in hashvalue2parameter:
-            hashvalue2parameter[hash_value] = parameter
+        parameter_strings = lines[i + 1].split()
+        parameters = []
+        for parameter_string in parameter_strings:
+            parameters.append(float(parameter_string))
+        parameters = np.array(parameters)
+        if hash_value not in hashvalue2parameters:
+            hashvalue2parameters[hash_value] = parameters
         else:
-            hashvalue2parameter[hash_value] += parameter
-    return hashvalue2parameter
+            hashvalue2parameters[hash_value] += parameters
+    return hashvalue2parameters
 
 if __name__ == "__main__":
     args = parse_args()
     hashvalue2polynomial = read_polynomial(args.polynomial)
-    hashvalue2parameter = read_parameter(args.parameter)
+    hashvalue2parameters = read_parameter(args.parameter)
+    nhiddens = len(hashvalue2parameters[[*hashvalue2parameters.keys()][0]])
     with open(args.output, 'w') as f:
         # print everything except bias
         for hashvalue in hashvalue2polynomial:
             if hashvalue != 0:
                 polynomial = hashvalue2polynomial[hashvalue]
-                if hashvalue in hashvalue2parameter:
-                    parameter = hashvalue2parameter[hashvalue]
+                if hashvalue in hashvalue2parameters:
+                    parameters = hashvalue2parameters[hashvalue]
                 else:
-                    parameter = 0.0
+                    parameters = np.zeros(nhiddens, dtype=float)
                 print(polynomial, end='', file=f)
-                print("%25.15e" % parameter, file=f)
+                for i in range(len(parameters)):
+                    print("%25.15e" % parameters[i], end='', file=f)
+                print(file=f)
         # print bias if exists
-        if 0 in hashvalue2parameter:
+        if 0 in hashvalue2parameters:
             print("bias", file=f)
-            print("%25.15e" % hashvalue2parameter[0], file=f)
+            parameters = hashvalue2parameters[0]
+            for i in range(len(parameters)):
+                print("%25.15e" % parameters[i], end='', file=f)
+            print(file=f)
